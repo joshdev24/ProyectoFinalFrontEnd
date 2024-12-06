2e4956cdc5072819bceaf3f1e8e48ac4aa7eef74
josh.dev <joshtowson16@gmail.com>
2024-12-06 01:10:41 -0300

    Despliegue

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { useContext, createContext, useState, useEffect } from "react";
 export const AuthContext  = createContext()
 
 export const AuthContextProvider = ({children}) =>{
-    const access_token = sessionStorage.getItem('access_token')
+    const access_token = sessionStorage.getItem("access_token")
     //Estado booleano
     const [isAuthenticatedUser, setIsAuthenticatedUser] = useState(
         Boolean(access_token)
@@ -11,7 +11,7 @@ export const AuthContextProvider = ({children}) =>{
 
     useEffect(
         () => {
-            const access_token = sessionStorage.getItem('access_token')
+            const access_token = sessionStorage.getItem("access_token")
             if(access_token) {
                 setIsAuthenticatedUser(true)
             }
