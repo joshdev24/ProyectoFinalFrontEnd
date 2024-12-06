d179f67c48a5b3961ef9997a0eb8b5ccb6edb531
josh.dev <joshtowson16@gmail.com>
2024-12-06 00:16:56 -0300

    Despliegue

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { useAuthContext } from '../Context/AuthContext'
-
-
 //Nos llama a la ruta hija o nesteada de nuestra ruta
 import { Navigate, Outlet } from 'react-router-dom'
-
+import { useLocation } from 'react-router-dom';
 const ProtectedRoute = () => {
-    const {isAuthenticatedUser} = useAuthContext()
-    
-  return (
-    isAuthenticatedUser ? <Outlet/> : <Navigate to={'/login'}/>
-  )
-}
+    const { isAuthenticatedUser } = useAuthContext();
+    const location = useLocation();
+
+    return isAuthenticatedUser ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/login" state={{ from: location }} />
+    );
+};
 
 export default ProtectedRoute
-
