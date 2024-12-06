f758924d013d27bb7e281bad0174a4b538d20a21
josh.dev <joshtowson16@gmail.com>
2024-12-06 01:03:21 -0300

    Despliegue

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -8,17 +8,9 @@ import { Navigate, Outlet } from 'react-router-dom'
 const ProtectedRoute = () => {
     const {isAuthenticatedUser} = useAuthContext()
     
-    console.log('isAuthenticatedUser es:', isAuthenticatedUser)
-    
-    if (isAuthenticatedUser === null || isAuthenticatedUser === undefined) {
-        throw new Error('isAuthenticatedUser is null or undefined')
-    }
-    
   return (
     isAuthenticatedUser ? <Outlet/> : <Navigate to={'/login'}/>
   )
 }
-
-
 
 export default ProtectedRoute
