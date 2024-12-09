85ea6b34366fc5db018dd4bc51a29df4b534be33
josh.dev <joshtowson16@gmail.com>
2024-12-09 19:07:18 -0300

    links del mail

--- src/screens/ResetPassword/ResetPassword.jsx
+++ src/screens/ResetPassword/ResetPassword.jsx
@@ -1,44 +1,39 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { extractFormData } from '../../utils/extractFormData'
-import { PUT, getUnnauthenticatedHeaders } from '../../fetching/http.fetching'
+import { getUnnauthenticatedHeaders, PUT } from '../../fetching/http.fetching'
 import "./ResetPassword.css"
 import { useState } from 'react'
+import ENVIROMENT from '../../../enviroment'
 
 
 const ResetPassword = () => {
-
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState('')
     const { reset_token } = useParams()
-    const [errors, setErrors] = useState('')
-    const [responseFetch, setResponseFetch] = useState('')
 
-    const formSchema = {
-        'password': '',
-    }
-    const { form_values_state, handleChangeInputValue } = useForm(formSchema)
-
-    const handleSubmitResetPasswordForm = async (e) => {
+    const handleSubmitResetForm = async (e) => {
         try {
-            e.preventDefault()
-            if (form_values_state.password !== form_values_state.confirm_password) {
-                return setErrors('Passwords do not match')
-            } else {
-                setErrors('')
-            }
-            const response = await PUT(`${ENVIROMENT.URL_BACKEND}/api/auth/reset-password/${reset_token}`, {
-                headers: getUnnauthenticatedHeaders(),
-                body: JSON.stringify(form_values_state)
-            })
-            if (!response.ok) {
-                return setErrors(response.payload.detail)
-            } else {
-                setResponseFetch(response.message)
-            }
-            console.log({ response })
+        e.preventDefault()
+        const form_HTML = e.target
+        const form_Values = new FormData(form_HTML)
+        const form_fields = {
+            'password': ''
         }
-        catch (error) {
-            error.message
-        }
+        const form_values_object = extractFormData(form_fields, form_Values)
+        const response = await PUT(`${ENVIROMENT.URL_BACKEND}/api/auth/reset-password/` + reset_token, {
+            headers: getUnnauthenticatedHeaders(),
+				body: JSON.stringify(form_values_object)
+			})
+            if (response.ok) {
+                setSuccess('Contraseña restablecida con exito')
+            }
+			console.log({response})
+		}
+		catch(error){
+			setError('Error al restablecer contraseña')
+			
+		}
     }
 
     return (
@@ -47,7 +42,7 @@ const ResetPassword = () => {
     <p class="reset-password-subtitle">
         Ingresa tu nueva contraseña para recuperar el acceso.
     </p>
-    <form class="reset-password-form" onSubmit={handleSubmitResetPasswordForm}>
+    <form class="reset-password-form" onSubmit={handleSubmitResetForm}>
         <label for="password" class="input-label">Nueva Contraseña</label>
         <input 
             type="password" 
@@ -56,7 +51,6 @@ const ResetPassword = () => {
             class="input-field" 
             placeholder="Escribe tu nueva contraseña" 
             required 
-            onChange={handleChangeInputValue}
         />
         <button type="submit" class="reset-button">Restablecer Contraseña</button>
     </form>
@@ -67,8 +61,8 @@ const ResetPassword = () => {
             <a href="/register">Crear una cuenta</a>
         </p>
     </div>
-        {error && <p style={{ color: 'red' }}>{errors}</p>} 
-				{success && <p style={{ color: 'green' }}>{responseFetch}</p>} 
+        {error && <p style={{ color: 'red' }}>{error}</p>} 
+				{success && <p style={{ color: 'green' }}>{success}</p>} 
     
     </div>
     )
