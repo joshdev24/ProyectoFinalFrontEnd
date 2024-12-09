05e3eea9c4235ac0791776e0c7546df7b4164bc3
josh.dev <joshtowson16@gmail.com>
2024-12-09 18:23:19 -0300

    links del mail

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthContextProvider } from './Context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-    
-  </BrowserRouter>
+  </HashRouter>
 )
+
