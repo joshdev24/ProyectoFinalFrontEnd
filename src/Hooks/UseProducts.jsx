1de935e50bff10ab64f789ed4d89240b313ed35f
josh.dev <joshtowson16@gmail.com>
2024-12-10 21:39:29 -0300

    links del mail

--- src/Hooks/UseProducts.jsx
+++ src/Hooks/UseProducts.jsx
@@ -11,6 +11,7 @@ const useProducts = () => {
     const getProducts = async () => {
         const response = await GET(`${ENVIROMENT.URL_BACKEND}/api/products`, {
             headers: getAuthenticatedHeaders(),
+            mode: "no-cors",
         })
     
         console.log({response})
