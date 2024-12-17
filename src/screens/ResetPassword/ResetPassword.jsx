2249d8922749547ba68a29e88a7d6bb9b81c92ec
josh.dev <joshtowson16@gmail.com>
2024-12-17 19:03:13 -0300

    resetar contraseña

--- src/screens/ResetPassword/ResetPassword.jsx
+++ src/screens/ResetPassword/ResetPassword.jsx
@@ -22,7 +22,6 @@ const ResetPassword = () => {
 
             const response = await GET(`${ENVIROMENT.URL_BACKEND}/api/auth/reset-password/${resetToken}`, {
                 headers: getAuthenticatedHeaders(),
-                body: JSON.stringify({token: resetToken}),
             });
 
             if (response === null || response === undefined) {
