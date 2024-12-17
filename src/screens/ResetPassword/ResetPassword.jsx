ad86042b8894011be3cacc17d86f892ef9239029
josh.dev <joshtowson16@gmail.com>
2024-12-17 03:57:46 -0300

    resetar contraseña

--- src/screens/ResetPassword/ResetPassword.jsx
+++ src/screens/ResetPassword/ResetPassword.jsx
@@ -20,7 +20,7 @@ const ResetPassword = () => {
                 throw new Error("Por favor, introduce un token válido.");
             }
 
-            const response = await POST(`${ENVIROMENT.URL_BACKEND}/api/auth/verify/${resetToken}`, {
+            const response = await POST(`${ENVIROMENT.URL_BACKEND}/api/auth/reset-password/${resetToken}`, {
                 headers: getAuthenticatedHeaders(),
                 body: JSON.stringify({ token: resetToken }),
             });
